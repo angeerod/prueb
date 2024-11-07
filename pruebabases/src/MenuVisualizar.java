782324cb7b98ea92460bde6c681d60cf1e33eeb0
AnthonyConejo <anthony.conejoalvarado@ucr.ac.cr>
2024-11-06 21:11:27 -0600

    Revert "Update MenuVisualizar.java"
    This reverts commit 0a9038fe5547f5579b0f4d9df255ad57edd676c7.

--- pruebabases/src/MenuVisualizar.java
+++ pruebabases/src/MenuVisualizar.java
@@ -36,13 +36,6 @@ public class MenuVisualizar extends JFrame {
 
         JButton btnVisualizarCargo = new JButton("Visualizar Cargos");
         btnVisualizarCargo.setBounds(200, 180, 200, 40); // Coordenadas para el botón de visualizar cargo
-        btnVisualizarCargo.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Al hacer clic, abrir la ventana de consultar cargo
-                abrirVisualizarCargo();
-            }
-        });
         add(btnVisualizarCargo); // Añadir el botón al JFrame
 
         JButton btnVisualizarPedido = new JButton("Visualizar Pedidos");
@@ -63,11 +56,6 @@ public class MenuVisualizar extends JFrame {
         }
     });
     add(btnRegresar); // Añadir el botón al JFrame
-}
-
-private void abrirVisualizarCargo() {
-    VisualizarCargo visualizarCargo = new VisualizarCargo();
-    visualizarCargo.setVisible(true);
 }
 
 // Método para regresar al menú inicial
